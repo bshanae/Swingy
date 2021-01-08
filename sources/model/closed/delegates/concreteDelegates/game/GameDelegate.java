621b1a52cb98feeee9554f80572d7ed6fb30afbb
belchenkovova <49374370+belchenkovova@users.noreply.github.com>
2021-01-08 22:09:43 +0300

    Show message, when going on new map

--- sources/model/closed/delegates/concreteDelegates/game/GameDelegate.java
+++ sources/model/closed/delegates/concreteDelegates/game/GameDelegate.java
@@ -1,23 +1,62 @@
 package model.closed.delegates.concreteDelegates.game;
 
+import model.closed.Session;
 import model.closed.delegates.abstractDelegate.AbstractDelegate;
 import model.closed.delegates.abstractDelegate.AbstractResolutionObject;
 import model.closed.delegates.abstractDelegate.commands.ExecutableCommand;
+import model.closed.delegates.concreteDelegates.common.InfoDelegate;
 import model.closed.delegates.concreteDelegates.game.map.MapDelegate;
 
 public class				GameDelegate extends AbstractDelegate
 {
+// -----------------------> Nested types
+
 	public static class		ResolutionObject implements AbstractResolutionObject {}
+
+// -----------------------> Attributes
+
+	private boolean			isFirstActivation;
+
+// -----------------------> Constructor
+
+	public 					GameDelegate()
+	{
+		isFirstActivation = true;
+	}
+
+// -----------------------> Implementations
 
 	@Override
 	public void				whenActivated()
 	{
-		stackChildLater(new MapDelegate());
+		if (isFirstActivation)
+		{
+			showMap();
+			isFirstActivation = false;
+		}
 	}
 
 	@Override
-	public void				tryToExecuteCommand(ExecutableCommand command)
+	protected void			whenChildResolved(AbstractResolutionObject object)
 	{
-		resolveLater(new ResolutionObject());
+		if (object instanceof InfoDelegate.ResolutionObject)
+			showMap();
+		else if (object instanceof MapDelegate.ResolutionObject)
+			showInfo();
+	}
+
+// -----------------------> Private methods
+
+	private void			showMap()
+	{
+		stackChildLater(new MapDelegate());
+	}
+
+	private void			showInfo()
+	{
+		String				message;
+
+		message = String.format("Generating map of level %s...", Session.getInstance().getHero().getLevel());
+		stackChildLater(new InfoDelegate(message));
 	}
 }
